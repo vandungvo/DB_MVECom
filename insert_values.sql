80a359eb81ed2bc3c7b235cae1c2a1280dc4854d
Dung Vo <dung.vovandung@hcmut.edu.vn>
2023-12-09 21:53:27 +0700

    update

--- insert_values.sql
+++ insert_values.sql
@@ -1,42 +1,41 @@
 use e_commerce;
 set global local_infile = true;
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/users.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/users.csv' 
 INTO TABLE users
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (user_id,first_name,last_name,user_type,email,user_password);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/customer.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/customer.csv' 
 INTO TABLE customer
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (user_id);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shop.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/shop.csv' 
 INTO TABLE shop
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (user_id,shop_name,address);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipping_company.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/shipping_company.csv' 
 INTO TABLE shipping_company
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (company_id,company_name,address,phone_num);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipper.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/shipper.csv' 
 INTO TABLE shipper
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/category.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/category.csv' 
 INTO TABLE category
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (ctg_id,ctg_name);
 
-delete from product;
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/product.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/product.csv' 
 INTO TABLE product
 character set latin1
 FIELDS TERMINATED BY ',' 
@@ -44,19 +43,19 @@ lines terminated by '\r\n'
 IGNORE 1 ROWS
 (product_id,shop_id,ctg_id,product_name,SKU,upload_date,price,stock,sold_quantities,product_description,image,rating,rate_nums);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/orders.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/orders.csv' 
 INTO TABLE orders
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
 (order_id,cus_id,total_price,order_date);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/bill.csv' 
 INTO TABLE bill
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS 
 (bill_id, order_id, shop_id, order_date, total_price, bill_status);
 
-LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill_product.csv' 
+LOAD DATA LOCAL INFILE 'C:/Users/Admin/OneDrive/Desktop/DB_MVECom/data/bill_product.csv' 
 INTO TABLE bill_product
 FIELDS TERMINATED BY ',' 
 IGNORE 1 ROWS
