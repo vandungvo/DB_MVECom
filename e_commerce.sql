24d015043c04a5373c6aa793806c4f9f786135c7
Dung Vo <dung.vovandung@hcmut.edu.vn>
2023-12-09 21:15:09 +0700

    update

--- e_commerce.sql
+++ e_commerce.sql
@@ -4,7 +4,6 @@ use e_commerce;
 
 drop table if exists refund;
 drop table if exists review;
-drop table if exists review;
 drop table if exists shipment_address;
 drop table if exists shipment;
 drop table if exists bill_product;
@@ -26,29 +25,50 @@ drop table if exists customer;
 drop table if exists users;
 
 create table users (
+<<<<<<< HEAD
 	user_id int not null primary key,
+=======
+	user_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     first_name varchar(30) not null,
     last_name varchar(30) not null,
 	user_type varchar(10) not null,
     email varchar(100) not null,
+<<<<<<< HEAD
     password varchar(127) not null,
+=======
+    user_password varchar(32) not null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     constraint user_type check (user_type in ('CUSTOMER', 'SELLER', 'SHIPPER', 'ADMIN'))
 );
 
 create table customer (
+<<<<<<< HEAD
 	user_id int not null primary key,
+=======
+	user_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     constraint user_customer_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade
 );
 
 create table shipping_company (
+<<<<<<< HEAD
 	company_id int not null primary key,
     name varchar(100) not null,
+=======
+	company_id int not null primary key auto_increment,
+    company_name varchar(100) not null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     address text,
     phone_num varchar(12) not null unique
 );
 
 create table shipper (
+<<<<<<< HEAD
 	user_id int not null primary key,
+=======
+	user_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     company_id int not null,
 	phone_num varchar(12) not null unique,
     constraint user_shipper_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade,
@@ -56,14 +76,18 @@ create table shipper (
 );
 
 create table shop (
+<<<<<<< HEAD
 	user_id int not null primary key,
+=======
+	user_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     shop_name varchar(100) not null,
-    /*xem xet address */
     address text,
     constraint user_shop_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade
 ); 
 
 create table category (
+<<<<<<< HEAD
 	ctg_id int not null primary key,
     name varchar(100) not null
 );
@@ -73,57 +97,93 @@ create table product (
     shop_id int not null,
     ctg_id int not null,
     name varchar(1000) not null,
+=======
+	ctg_id int not null primary key auto_increment,
+    ctg_name varchar(100) not null
+);
+
+create table product (
+
+	product_id int not null primary key auto_increment,
+    shop_id int not null,
+    ctg_id int not null,
+    product_name varchar(1000) not null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     SKU varchar(10) not null unique,
     upload_date date not null default (curdate()),
 	price decimal(14,2) not null,
+
+
     stock int not null,
 	sold_quantities int not null default 0,
-    description text,
+    product_description text,
     image text,
+
+
     rating decimal(2, 1) default 0,
 	rate_nums int default 0,
     constraint shop_product_fk foreign key (shop_id) references shop (user_id) on update cascade on delete cascade, 
     constraint ctg_product_fk foreign key (ctg_id) references category (ctg_id) on update cascade on delete no action
+
+
 );
 
-/* kiem tra lai weak entity*/
 create table wish_item (
+<<<<<<< HEAD
 	wish_id int not null primary key,
+=======
+	wish_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     cus_id int not null,
     product_id int not null,
     constraint cus_wish_fk foreign key (cus_id) references customer (user_id) on update cascade on delete cascade,
     constraint product_wish_fk foreign key (product_id) references product (product_id)
 );
 
-/*kiem tra lai phan nay*/
 create table promotion (
+<<<<<<< HEAD
 	promotion_id int not null primary key,
     name varchar(100) not null,
     /* xem lai start_date*/
+=======
+	promotion_id varchar(20) not null primary key,
+    promotion_name varchar(100) not null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     start_date date not null,
     end_date date not null,
     promotion_type varchar (16) not null,
-    /* kiem tra lai discount */
     discount_percen int not null,
     constraint promotion_type check (promotion_type in ('VOUCHER', 'SALE', 'SHOP PROMOTION'))
 );
 
 create table shop_promotion (
+<<<<<<< HEAD
 	promotion_id int not null,
+=======
+	promotion_id varchar(20) not null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     shop_id int not null,
     constraint promotion_fk foreign key  (promotion_id) references promotion (promotion_id) on update cascade on delete no action, 
     constraint shop_promotion_fk foreign key (shop_id) references shop (user_id) on update cascade on delete cascade
 );
 
 create table sale (
+<<<<<<< HEAD
 	sale_id int not null primary key,
+=======
+	sale_id varchar(20) not null primary key,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     product_id int not null,
     constraint promotion_sale_fk foreign key (sale_id) references promotion (promotion_id) on update cascade on delete no action,
     constraint product_sale_fk foreign key (product_id) references product (product_id) on update cascade on delete cascade
 );
 
 create table voucher (
+<<<<<<< HEAD
 	voucher_id int not null primary key,
+=======
+	voucher_id varchar(20) not null primary key,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     cus_id int not null,
     voucher_code varchar(50) not null,
     max_amount int not null,
@@ -132,10 +192,13 @@ create table voucher (
 );
 
 create table orders (
+<<<<<<< HEAD
 	order_id int not null primary key,
+=======
+	order_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     cus_id int not null,
     total_price decimal(12, 2) not null,
-    /* chu y kieu du lieu date*/
     order_date date not null default(curdate()),
     constraint customer_order_fk foreign key (cus_id) references customer (user_id) on update cascade on delete cascade
 );
@@ -156,26 +219,29 @@ values
 create table payment (
 	order_id int not null,
     amount decimal(12,2) not null,
-    /* thieu method*/
     method_id int not null,
-    /*thieu timestamp*/
-    timestamp datetime not null default current_timestamp,
+    payment_timestamp datetime not null default current_timestamp,
     constraint method_payment_fk foreign key (method_id) references method (method_id) on update no action on delete no action
 );
 
 create table bill (
+<<<<<<< HEAD
 	bill_id int not null primary key,
     order_id int not null,
     shop_id int not null,
     voucher_id int,
+=======
+	bill_id int not null primary key auto_increment,
+    order_id int not null,
+    shop_id int not null,
+    voucher_id varchar(20) default null,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     order_date date not null default (curdate()),
     total_price decimal(12,2) not null,
-    /* thieu thuoc tinh status*/
-    status varchar(32) not null,
+    bill_status varchar(35) not null,
     constraint oder_bill_fk foreign key (order_id) references orders (order_id) on update cascade on delete cascade,
     constraint shop_bill_fk foreign key (shop_id) references shop (user_id) on update no action on delete no action,
-    constraint voucher_bill_fk foreign key (voucher_id) references voucher (voucher_id) on update no action on delete no action,
-    constraint bill_status check (status in ('WAIT FOR CONFIRMATION', 'DELIVERING', 'COMPLETE', 'INCOMPLETE'))
+    constraint voucher_bill_fk foreign key (voucher_id) references voucher (voucher_id) on update cascade on delete cascade
 );
 
 create table bill_product (
@@ -188,24 +254,31 @@ create table bill_product (
 );
 
 create table shipment (
+<<<<<<< HEAD
 	shipment_id int not null primary key,
     voucher_id int,
+=======
+	shipment_id int not null primary key auto_increment,
+    voucher_id varchar(20),
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     bill_id int not null,
     shipper_id int not null,
     shipping_free decimal(10,2) not null,
     phone_num varchar(12) not null,
-    /* kiem tra lai state*/
-    status varchar(12) not null,
+    shipment_status varchar(12) not null,
     estimated_time date default (curdate()),
     constraint voucher_shipment_fk foreign key (voucher_id) references voucher (voucher_id) on update no action on delete no action,
     constraint bill_shipment_fk foreign key (bill_id) references bill (bill_id) on update no action on delete cascade,
     constraint shipper_shipment_fk foreign key (shipper_id) references shipper (user_id) on update no action on delete no action,
-    constraint shipment_status check (status in ('COMPLETE', 'INCOMPLETE', 'DELIVERING'))
+    constraint shipment_status check (shipment_status in ('COMPLETE', 'INCOMPLETE', 'DELIVERING'))
 );
 
 create table shipment_address (
 	shipment_id int not null,
+<<<<<<< HEAD
     /* thieu cac thuoc tinh */
+=======
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     street_address varchar(50),
     district varchar(20),
     city varchar(20) not null,
@@ -214,29 +287,41 @@ create table shipment_address (
 );
 
 create table review (
+<<<<<<< HEAD
 	review_id int not null primary key,
+=======
+	review_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     cus_id int not null,
     product_id int not null,
     post_date datetime not null default current_timestamp,
     rating decimal(2,1),
-    comment text,
+    review_comment text,
     constraint customer_review_fk foreign key (cus_id) references customer (user_id),
     constraint product_review_fk foreign key (product_id) references product (product_id) on update no action on delete cascade,
     constraint rating_check check(rating >= 0 and rating <= 5)
 );
 
 create table refund (
+<<<<<<< HEAD
 	refund_id int not null primary key,
+=======
+	refund_id int not null primary key auto_increment,
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
     cus_id int not null,
     bill_id int not null,
     reason text,
     amount decimal(12,2) not null,
-    date date not null default (curdate()),
-    /* thieu state*/
-    status varchar(32) not null,
+    refund_date date not null default (curdate()),
+    refund_status varchar(32) not null,
     constraint customer_refund_fk foreign key (cus_id) references customer (user_id) on update no action on delete cascade,
     constraint bill_refund_fk foreign key (bill_id) references bill (bill_id) on update no action on delete no action,
+<<<<<<< HEAD
     constraint refund_status check (status in ('WAIT FOR CONFIRMATION', 'CONFIRMED', 'REFUSED'))
 );
 
 set foreign_key_checks = 0;
+=======
+    constraint refund_status check (refund_status in ('WAIT FOR CONFIRMATION', 'CONFIRMED', 'REFUSED'))
+);
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
--- insert_values.sql
+++ insert_values.sql
@@ -1,23 +1,28 @@
 use e_commerce;
+set global local_infile = true;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/users.csv' 
 INTO TABLE users
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id,first_name,last_name,user_type,email,user_password);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/customer.csv' 
 INTO TABLE customer
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shop.csv' 
 INTO TABLE shop
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id,shop_name,address);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipping_company.csv' 
 INTO TABLE shipping_company
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(company_id,company_name,address,phone_num);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipper.csv' 
 INTO TABLE shipper
@@ -27,27 +32,34 @@ IGNORE 1 ROWS;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/category.csv' 
 INTO TABLE category
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(ctg_id,ctg_name);
 
+delete from product;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/product.csv' 
 INTO TABLE product
 character set latin1
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+lines terminated by '\r\n'
+IGNORE 1 ROWS
+(product_id,shop_id,ctg_id,product_name,SKU,upload_date,price,stock,sold_quantities,product_description,image,rating,rate_nums);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/orders.csv' 
 INTO TABLE orders
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(order_id,cus_id,total_price,order_date);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill.csv' 
 INTO TABLE bill
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS 
+(bill_id, order_id, shop_id, order_date, total_price, bill_status);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill_product.csv' 
 INTO TABLE bill_product
 FIELDS TERMINATED BY ',' 
+<<<<<<< HEAD
 IGNORE 1 ROWS;
 
 -- Insert data into 'users' table
@@ -134,3 +146,7 @@ VALUES (1, 1, 1, 'Product damaged', 30.00, '2023-12-10', 'WAIT FOR CONFIRMATION'
 SET foreign_key_checks = 1;
 
 
+=======
+IGNORE 1 ROWS
+(bill_id,product_id,quantity);
+>>>>>>> 84d2b02de39f5c4c18dad3b6e164edbe3832f860
