0556b6aa4494a943863551e1c742c6b00cc60052
dang khoa <khuynhledang@gmail.com>
2023-12-09 14:39:22 +0700

    update data

--- Database/e_commerce.sql
+++ Database/e_commerce.sql
@@ -26,7 +26,7 @@ drop table if exists customer;
 drop table if exists users;
 
 create table users (
-	user_id varchar(40) not null primary key,
+	user_id int not null primary key auto_increment,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
 	user_type varchar(10) not null,
@@ -36,69 +36,72 @@ create table users (
 );
 
 create table customer (
-	user_id varchar(40) not null primary key,
+	user_id int not null primary key auto_increment,
     constraint user_customer_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade
 );
 
 create table shipping_company (
-	company_id varchar(40) not null primary key,
+	company_id int not null primary key auto_increment,
     name varchar(100) not null,
     address text,
     phone_num varchar(12) not null unique
 );
 
 create table shipper (
-	user_id varchar(40) not null primary key,
-    company_id varchar(40) not null,
+	user_id int not null primary key auto_increment,
+    company_id int not null,
 	phone_num varchar(12) not null unique,
     constraint user_shipper_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade,
     constraint company_shipper_fk foreign key (company_id) references shipping_company (company_id) 
 );
 
 create table shop (
-	user_id varchar(40) not null primary key,
+	user_id int not null primary key auto_increment,
     shop_name varchar(100) not null,
     address text,
     constraint user_shop_fk foreign key (user_id) references users (user_id) on update cascade on delete cascade
 ); 
 
 create table category (
-	ctg_id varchar(40) not null primary key,
+	ctg_id int not null primary key auto_increment,
     name varchar(100) not null
 );
 
 create table product (
 
-	product_id varchar(40) not null primary key auto_increment,
-    shop_id varchar(40) not null,
-    ctg_id varchar(40) not null,
+	product_id int not null primary key auto_increment,
+    shop_id int not null,
+    ctg_id int not null,
     name varchar(1000) not null,
     SKU varchar(10) not null unique,
     upload_date date not null default (curdate()),
 	price decimal(14,2) not null,
+
 
     stock int not null,
 	sold_quantities int not null default 0,
     description text,
     image text,
 
+
     rating decimal(2, 1) default 0,
 	rate_nums int default 0,
     constraint shop_product_fk foreign key (shop_id) references shop (user_id) on update cascade on delete cascade, 
     constraint ctg_product_fk foreign key (ctg_id) references category (ctg_id) on update cascade on delete no action
 
+
 );
 
 create table wish_item (
-	wish_id varchar(40) not null primary key,
-    cus_id varchar(40) not null,
-    product_id varchar(40) not null,
+	wish_id int not null primary key auto_increment,
+    cus_id int not null,
+    product_id int not null,
     constraint cus_wish_fk foreign key (cus_id) references customer (user_id) on update cascade on delete cascade,
     constraint product_wish_fk foreign key (product_id) references product (product_id)
 );
 
 create table promotion (
-	promotion_id varchar(40) not null primary key,
+	promotion_id varchar(20) not null primary key,
     name varchar(100) not null,
     start_date date not null,
     end_date date not null,
@@ -108,22 +111,22 @@ create table promotion (
 );
 
 create table shop_promotion (
-	promotion_id varchar(40) not null,
-    shop_id varchar(40) not null,
+	promotion_id varchar(20) not null,
+    shop_id int not null,
     constraint promotion_fk foreign key  (promotion_id) references promotion (promotion_id) on update cascade on delete no action, 
     constraint shop_promotion_fk foreign key (shop_id) references shop (user_id) on update cascade on delete cascade
 );
 
 create table sale (
-	sale_id varchar(40) not null primary key,
-    product_id varchar(40) not null,
+	sale_id varchar(20) not null primary key,
+    product_id int not null,
     constraint promotion_sale_fk foreign key (sale_id) references promotion (promotion_id) on update cascade on delete no action,
     constraint product_sale_fk foreign key (product_id) references product (product_id) on update cascade on delete cascade
 );
 
 create table voucher (
-	voucher_id varchar(40) not null primary key,
-    cus_id varchar(40) not null,
+	voucher_id varchar(20) not null primary key,
+    cus_id int not null,
     voucher_code varchar(50) not null,
     max_amount int not null,
     constraint promotion_voucher_fk foreign key (voucher_id) references promotion (promotion_id) on update cascade on delete no action,
@@ -131,8 +134,8 @@ create table voucher (
 );
 
 create table orders (
-	order_id varchar(40) not null primary key,
-    cus_id varchar(40) not null,
+	order_id int not null primary key auto_increment,
+    cus_id int not null,
     total_price decimal(12, 2) not null,
     order_date date not null default(curdate()),
     constraint customer_order_fk foreign key (cus_id) references customer (user_id) on update cascade on delete cascade
@@ -152,7 +155,7 @@ values
 	(5, 'Cash on Delivery');
 
 create table payment (
-	order_id varchar(10) not null,
+	order_id int not null,
     amount decimal(12,2) not null,
     method_id int not null,
     timestamp datetime not null default current_timestamp,
@@ -160,22 +163,21 @@ create table payment (
 );
 
 create table bill (
-	bill_id varchar(40) not null primary key,
-    order_id varchar(40) not null,
-    shop_id varchar(40) not null,
-    voucher_id varchar(40),
+	bill_id int not null primary key auto_increment,
+    order_id int not null,
+    shop_id int not null,
+    voucher_id varchar(20) default null,
     order_date date not null default (curdate()),
     total_price decimal(12,2) not null,
-    status varchar(32) not null,
+    status varchar(35) not null,
     constraint oder_bill_fk foreign key (order_id) references orders (order_id) on update cascade on delete cascade,
     constraint shop_bill_fk foreign key (shop_id) references shop (user_id) on update no action on delete no action,
-    constraint voucher_bill_fk foreign key (voucher_id) references voucher (voucher_id) on update no action on delete no action,
-    constraint bill_status check (status in ('WAIT FOR CONFIRMATION', 'DELIVERING', 'COMPLETE', 'INCOMPLETE'))
+    constraint voucher_bill_fk foreign key (voucher_id) references voucher (voucher_id) on update cascade on delete cascade
 );
 
 create table bill_product (
-	bill_id varchar(40),
-    product_id varchar(40),
+	bill_id int,
+    product_id int,
     quantity int not null,
     constraint bil_product_fk primary key (bill_id, product_id),
     constraint bill_fk foreign key (bill_id) references bill (bill_id) on update no action on delete cascade,
@@ -183,10 +185,10 @@ create table bill_product (
 );
 
 create table shipment (
-	shipment_id varchar(40) not null primary key,
-    voucher_id varchar(40),
-    bill_id varchar(40) not null,
-    shipper_id varchar(40) not null,
+	shipment_id int not null primary key auto_increment,
+    voucher_id varchar(20),
+    bill_id int not null,
+    shipper_id int not null,
     shipping_free decimal(10,2) not null,
     phone_num varchar(12) not null,
     status varchar(12) not null,
@@ -198,7 +200,7 @@ create table shipment (
 );
 
 create table shipment_address (
-	shipment_id varchar(40) not null,
+	shipment_id int not null,
     street_address varchar(50),
     district varchar(20),
     city varchar(20) not null,
@@ -207,9 +209,9 @@ create table shipment_address (
 );
 
 create table review (
-	review_id varchar(40) not null primary key,
-    cus_id varchar(40) not null,
-    product_id varchar(40) not null,
+	review_id int not null primary key auto_increment,
+    cus_id int not null,
+    product_id int not null,
     post_date datetime not null default current_timestamp,
     rating decimal(2,1),
     comment text,
@@ -219,9 +221,9 @@ create table review (
 );
 
 create table refund (
-	refund_id varchar(40) not null primary key,
-    cus_id varchar(40) not null,
-    bill_id varchar(40) not null,
+	refund_id int not null primary key auto_increment,
+    cus_id int not null,
+    bill_id int not null,
     reason text,
     amount decimal(12,2) not null,
     date date not null default (curdate()),
--- insert_values.sql
+++ insert_values.sql
@@ -1,23 +1,28 @@
 use e_commerce;
+set global local_infile = true;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/users.csv' 
 INTO TABLE users
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id,first_name,last_name,user_type,email,password);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/customer.csv' 
 INTO TABLE customer
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shop.csv' 
 INTO TABLE shop
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(user_id,shop_name,address);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipping_company.csv' 
 INTO TABLE shipping_company
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(company_id,name,address,phone_num);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/shipper.csv' 
 INTO TABLE shipper
@@ -27,25 +32,33 @@ IGNORE 1 ROWS;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/category.csv' 
 INTO TABLE category
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(ctg_id,name);
 
+delete from product;
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/product.csv' 
 INTO TABLE product
 character set latin1
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+lines terminated by '\r\n'
+IGNORE 1 ROWS
+(product_id,shop_id,ctg_id,name,SKU,upload_date,price,stock,sold_quantities,description,image,rating,rate_nums);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/orders.csv' 
 INTO TABLE orders
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(order_id,cus_id,total_price,order_date);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill.csv' 
 INTO TABLE bill
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS 
+(bill_id, order_id, shop_id, order_date, total_price, status);
 
 LOAD DATA LOCAL INFILE 'c:/users/admin/Documents/hcsdl/DB_MVECom/data/bill_product.csv' 
 INTO TABLE bill_product
 FIELDS TERMINATED BY ',' 
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(bill_id,product_id,quantity);
+
